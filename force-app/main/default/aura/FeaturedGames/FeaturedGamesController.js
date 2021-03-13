845bfe90134772aae3d912b35d370288e5f0820c
exknown <abduong206@yahoo.com>
2021-03-12 18:18:34 -0800

    added featured games function

--- force-app/main/default/aura/FeaturedGames/FeaturedGamesController.js
+++ force-app/main/default/aura/FeaturedGames/FeaturedGamesController.js
@@ -1,5 +1,29 @@
 ({
     findPicture : function(component, event, helper) {
-        //let imgUrl = component.get("");
+        let imgUrl = component.get("c.getRandImgUrl");
+        let imgUrl1 = component.get("c.getRandImgUrl");
+        let imgUrl2 = component.get("c.getRandImgUrl");
+        imgUrl.setCallback(this, function(response) {
+            if(response.getState() == 'SUCCESS') {
+                component.set('v.gamePicture', response.getReturnValue());
+            }
+            
+            
+        })
+        imgUrl1.setCallback(this, function(response) {
+            if(response.getState() == 'SUCCESS') {
+                component.set('v.gamePicture1', response.getReturnValue());
+            }
+            
+        })
+        imgUrl2.setCallback(this, function(response) {
+            if(response.getState() == 'SUCCESS') {
+                component.set('v.gamePicture2', response.getReturnValue());
+            }
+            
+        })
+        $A.enqueueAction(imgUrl)
+        $A.enqueueAction(imgUrl1)
+        $A.enqueueAction(imgUrl2)
     }
 })
