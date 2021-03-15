6be5f9fd3fa838020e69bffa2f005bb0e10654ff
exknown <abduong206@yahoo.com>
2021-03-14 17:26:39 -0700

    redirect button

--- force-app/main/default/aura/redirectButton/redirectButtonController.js
+++ force-app/main/default/aura/redirectButton/redirectButtonController.js
@@ -1,11 +1,29 @@
 ({
-	goToNewPage: function(component, event, helper) {
-
-    var urlEvent = $A.get("e.force:navigateToURL");
-    urlEvent.setParams({
-        "url": "/recordlist/Champion__c/All"
-    });
-
-    urlEvent.fire();
-}
+    init : function(cmp, event, helper) {
+        var navService = cmp.find("navService");
+        // Sets the route to /lightning/o/Account/home
+        var pageReference = {
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Champion__c',
+                actionName: 'home'
+            }
+        };
+        cmp.set("v.pageReference", pageReference);
+        // Set the URL on the link or use the default if there's an error
+        var defaultUrl = "#";
+        navService.generateUrl(pageReference)
+            .then($A.getCallback(function(url) {
+                cmp.set("v.url", url ? url : defaultUrl);
+            }), $A.getCallback(function(error) {
+                cmp.set("v.url", defaultUrl);
+            }));
+    },
+    handleClick: function(cmp, event, helper) {
+        var navService = cmp.find("navService");
+        // Uses the pageReference definition in the init handler
+        var pageReference = cmp.get("v.pageReference");
+        event.preventDefault();
+        navService.navigate(pageReference);
+    }
 })
