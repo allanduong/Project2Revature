de43d7c363c96c84c86c4d7b8627fabddc75aa0c
exknown <abduong206@yahoo.com>
2021-03-16 13:20:55 -0700

    added the objects into the menifest files

--- force-app/main/default/aura/searchPlayerComponent/searchPlayerComponentController.js
+++ force-app/main/default/aura/searchPlayerComponent/searchPlayerComponentController.js
@@ -2,6 +2,9 @@
 	keyCheck : function(component, event, helper){
         if (event.which === 13) {
             let summonerName = component.find('sumName').get("v.value");
+            if(summonerName.includes(" ")){
+                summonerName = summonerName.replace(/\s/g, "%20");
+            }
             console.log(summonerName);
         }
         
